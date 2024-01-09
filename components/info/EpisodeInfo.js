ee5af16622f0ae4bf757eae8588e7a76de5a943e
Alan Varghese Paul <alanvarghesepaul29@gmail.com>
2024-01-09 17:05:52 +0530

    api update iframe

--- components/info/EpisodeInfo.js
+++ components/info/EpisodeInfo.js
@@ -14,10 +14,11 @@ const EpisodeInfo = (props) => {
       <div className="pt-2 pb-8 flex justify-center">
         <iframe
           className="w-4/5 aspect-video sm: pr-4 pl-4"
-          src={`https://autoembed.to/tv/tmdb/${seriesId}-${episodeDetails.season_number}-${episodeDetails.episode_number}`}
+          src={`https://vidsrc.to/embed/tv/${seriesId}/${episodeDetails.season_number}/${episodeDetails.episode_number}`}
           frameBorder={`0`}
           allowFullScreen={true}
         ></iframe>
+        {/* src={`https://autoembed.to/tv/tmdb/${seriesId}-${episodeDetails.season_number}-${episodeDetails.episode_number}`} */}
       </div>
       <div className="flex w-full items-center justify-center">
         <NextEpisode
--- components/info/MovieInfo.js
+++ components/info/MovieInfo.js
@@ -3,7 +3,7 @@ import ImageCard from "../display/ImageCard";
 import MovieDetails from "./MovieDetails";
 
 const MovieInfo = (props) => {
-  let {MovieDetail,genreArr,id}=props
+  let { MovieDetail, genreArr, id } = props;
   return (
     <div>
       <div className="flex flex-row flex-wrap place-content-center items-center mb-10 mt-5">
@@ -13,12 +13,13 @@ const MovieInfo = (props) => {
       <div className="pt-2 pb-8 flex justify-center">
         <iframe
           className="w-4/5 aspect-video"
-          src={"https://autoembed.to/movie/tmdb/" + id}
+          src={"https://vidsrc.to/embed/movie/" + id}
           title="YouTube video player"
           frameborder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
           allowFullScreen={true}
         ></iframe>
+        {/* src={"https://autoembed.to/movie/tmdb/" + id} */}
       </div>
     </div>
   );
