1ac9fdfd5021cecb672773730a79ad616cf8916e
kody816 <kodynott2@gmail.com>
2025-07-13 08:19:15 +0100

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,27 +1,29 @@
+// app/page.js
+
 import HomeDisplay from "@/components/display/HomeDisplay";
 import HomeFilter from "@/components/filter/HomeFilter";
 import SearchBar from "@/components/searchbar/SearchBar";
 import Title from "@/components/title/Title";
 
 async function getData() {
-  const apiKey = process.env.TMDB_API_KEY;
+  const apiKey = process.env.NEXT_PUBLIC_TMDB_API_KEY; // Use NEXT_PUBLIC prefix for .env access
   const resp = await fetch(
-    // `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US&page=1`
     `https://api.themoviedb.org/3/trending/all/day?api_key=${apiKey}&page=1`
   );
 
   if (!resp.ok) {
     throw new Error("Failed to fetch data");
   }
+
   const data = await resp.json();
-  let res = data.results;
-  return res;
+  return data.results;
 }
 
 export default async function Home() {
   const data = await getData();
+
   return (
-    <div className=" h-auto">
+    <div className="h-auto">
       <Title />
       {/* <SearchBar />
       <HomeFilter /> */}
