00625910604c5106acce44dda801a43a254c6fd0
kody816 <kodynott2@gmail.com>
2025-07-13 06:16:02 +0100

    MovieCards.js

--- components/display/MovieCards.js
+++ components/display/MovieCards.js
@@ -2,30 +2,27 @@ import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 
-const MovieCards = (props) => {
-  const { MovieCard } = props;
-  let poster_path = `https://image.tmdb.org/t/p/w342/${MovieCard.poster_path}`;
-  if (MovieCard.poster_path == null) {
-    poster_path = "https://i.imgur.com/wjVuAGb.png";
-  }
+const MovieCards = ({ MovieCard }) => {
+  const poster_path = MovieCard.poster_path
+    ? `https://image.tmdb.org/t/p/w342/${MovieCard.poster_path}`
+    : "https://i.imgur.com/wjVuAGb.png";
+
   return (
-    <div className="w-64 h-96 sm:w-52 sm:h-72 bg-grey m-3 hover:opacity-75 shadow-md">
-      <Link
-        key={MovieCard.id}
-        href="/movie/[id]"
-        as={`/movie/${MovieCard.id}`}
-        title={MovieCard.title}
-      >
-        <Image
-          src={poster_path}
-          alt={MovieCard.title}
-          className="rounded w-full h-full"
-          width={208}
-          height={288}
-          unoptimized
-        />
-      </Link>
-    </div>
+    <Link
+      key={MovieCard.id}
+      href={`/watch/${MovieCard.id}`}
+      title={MovieCard.title || MovieCard.name}
+      className="block w-[45%] sm:w-40 md:w-48 lg:w-52 xl:w-56 aspect-[2/3] rounded overflow-hidden bg-grey shadow-md hover:opacity-80 transition"
+    >
+      <Image
+        src={poster_path}
+        alt={MovieCard.title || MovieCard.name}
+        className="object-cover w-full h-full"
+        width={208}
+        height={312}
+        unoptimized
+      />
+    </Link>
   );
 };
 
