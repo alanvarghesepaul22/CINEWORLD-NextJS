152b7b6342738db312254e4c9788944961499d6d
kody816 <kodynott2@gmail.com>
2025-07-13 08:23:53 +0100

    Update page.js

--- app/series/[id]/page.js
+++ app/series/[id]/page.js
@@ -1,30 +1,28 @@
 import TvInfo from "@/components/info/TvInfo";
 
 export async function getData(id) {
-  const apiKey = process.env.API_KEY;
-  const res = await fetch(
-    `https://api.themoviedb.org/3/tv/${id}?api_key=${apiKey}`
-  );
-  let data = await res.json();
-  let genreArr = [];
+  const apiKey = process.env.NEXT_PUBLIC_TMDB_API_KEY;
 
-  data.genres.map((element) => {
-    genreArr.push(element.name);
-  });
+  const res = await fetch(
+    `https://api.themoviedb.org/3/tv/${id}?api_key=${apiKey}&language=en-US`
+  );
 
   if (!res.ok) {
-    throw new Error("Failed to Fetch data");
+    throw new Error("Failed to fetch TV data");
   }
+
+  const data = await res.json();
+  const genreArr = data.genres?.map((genre) => genre.name) || [];
 
   return { data, genreArr, id };
 }
-const TvDetail = async ({ params }) => {
-  let { data, genreArr, id } = await getData(params.id);
-  return (
-    <>
-      <TvInfo TvDetail={data} genreArr={genreArr} id={id} />
-    </>
-  );
-};
 
-export default TvDetail;
+export default async function TvDetail({ params }) {
+  const { data, genreArr, id } = await getData(params.id);
+
+  return (
+    <div className="p-4 max-w-screen-lg mx-auto">
+      <TvInfo TvDetail={data} genreArr={genreArr} id={id} />
+    </div>
+  );
+}
