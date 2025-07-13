aaee967ace9e19babc3789bdcd364a5536b657bc
kody816 <kodynott2@gmail.com>
2025-07-13 10:38:33 +0100

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
-import SectionRow from "@/components/display/SectionRow";
+// app/page.js
+import HorizontalSection from "@/components/display/HorizontalSection";
 import Title from "@/components/title/Title";
 
 async function getSection(url) {
   const apiKey = process.env.NEXT_PUBLIC_TMDB_API_KEY;
-  const res = await fetch(`https://api.themoviedb.org/3${url}?api_key=${apiKey}&language=en-US`);
-  if (!res.ok) throw new Error("Failed to fetch");
+  const res = await fetch(
+    `https://api.themoviedb.org/3${url}?api_key=${apiKey}&language=en-US`
+  );
   const data = await res.json();
   return data.results;
 }
@@ -13,15 +15,15 @@ export default async function Home() {
   const [trendingMovies, trendingSeries, newReleases] = await Promise.all([
     getSection("/trending/movie/week"),
     getSection("/trending/tv/week"),
-    getSection("/movie/now_playing")
+    getSection("/movie/now_playing"),
   ]);
 
   return (
     <div className="h-auto">
       <Title />
-      <SectionRow title="Trending Movies" movies={trendingMovies} link="/trending/movie" />
-      <SectionRow title="Trending Series" movies={trendingSeries} link="/trending/tv" />
-      <SectionRow title="New Releases" movies={newReleases} link="/new-releases" />
+      <HorizontalSection title="Trending Movies" movies={trendingMovies} />
+      <HorizontalSection title="Trending Series" movies={trendingSeries} />
+      <HorizontalSection title="New Releases" movies={newReleases} />
     </div>
   );
 }
