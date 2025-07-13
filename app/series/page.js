03c4fd7ed7707cb8428dd3971008eee07729e68e
kody816 <kodynott2@gmail.com>
2025-07-13 08:20:40 +0100

    Update page.js

--- app/series/page.js
+++ app/series/page.js
@@ -1,3 +1,5 @@
+// app/tv/page.js
+
 import TvDisplay from "@/components/display/TvDisplay";
 import HomeFilter from "@/components/filter/HomeFilter";
 import SearchBar from "@/components/searchbar/SearchBar";
@@ -5,7 +7,7 @@ import TvTitle from "@/components/title/TvTitle";
 import React from "react";
 
 async function getData() {
-  const apiKey = process.env.TMDB_API_KEY;
+  const apiKey = process.env.NEXT_PUBLIC_TMDB_API_KEY; // ✅ Use the correct env variable
   const resp = await fetch(
     `https://api.themoviedb.org/3/trending/tv/day?api_key=${apiKey}&page=1`
   );
@@ -13,22 +15,21 @@ async function getData() {
   if (!resp.ok) {
     throw new Error("Failed to fetch data");
   }
+
   const data = await resp.json();
-  let res = data.results;
-  return res;
+  return data.results;
 }
 
 const Series = async () => {
   const data = await getData();
+
   return (
-    <>
-      <div className=" h-auto">
-        <TvTitle />
-        {/* <SearchBar />
-        <HomeFilter /> */}
-        <TvDisplay series={data} />
-      </div>
-    </>
+    <div className="h-auto">
+      <TvTitle />
+      {/* <SearchBar />
+      <HomeFilter /> */}
+      <TvDisplay series={data} />
+    </div>
   );
 };
 
