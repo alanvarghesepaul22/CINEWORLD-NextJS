88376b6c4ba8636142487782ea23132f2364ba25
IGIRANEZA Patrick <168905940+Igiraneza18021@users.noreply.github.com>
2025-07-28 21:06:52 +0200

    Update Title.js

--- components/title/Title.js
+++ components/title/Title.js
@@ -4,7 +4,7 @@ const Title = () => {
   return (
     <div className="flex place-content-center mt-5 mx-5">
       <h1 className="w-full md:w-1/2 lg:w-1/3 text-white text-xl sm:text-2xl md:text-3xl text-center font-semibold">
-        <span className="text-primary">Cine</span>matic adventures await, start{" "}
+        <span className="text-primary">Cine</span>matic adventures await, free movie streamin start{" "}
         <span className="text-primary">stream</span>ing now
       </h1>
     </div>
