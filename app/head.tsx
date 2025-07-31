5f88da4aecb8838b25e2cce7c09c50a46f2805b3
IGIRANEZA Patrick <168905940+Igiraneza18021@users.noreply.github.com>
2025-07-31 13:48:39 +0200

    Create head.tsx

---
+++ app/head.tsx
@@ -0,0 +1,15 @@
+// app/head.tsx
+export default function Head() {
+  return (
+    <>
+      <title>PiStream free movie streaming</title>
+      <meta name="description" content="A World of Cinema" />
+      {/* AdSense Verification Script */}
+      <script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9433286911477345"
+        crossOrigin="anonymous"
+      ></script>
+    </>
+  );
+}
