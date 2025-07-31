213756de33f598d4cc7f21ca6520a9f06f9916ff
IGIRANEZA Patrick <168905940+Igiraneza18021@users.noreply.github.com>
2025-07-31 13:34:39 +0200

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import Header from "@/components/navbar/Header";
 import "./globals.css";
 import { Inter } from "next/font/google";
 import Footer from "@/components/footer/Footer";
+import Script from "next/script"; // ✅ Required for loading scripts
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,10 +14,19 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        {/* ✅ Google AdSense script for verification */}
+        <Script
+          id="adsense-verification"
+          strategy="beforeInteractive"
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9433286911477345"
+          crossOrigin="anonymous"
+        />
+      </head>
       <body className={inter.className}>
         <Header />
         <div className="mt-32">{children}</div>
-
         <Footer />
       </body>
     </html>
